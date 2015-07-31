6678c4726b976b0dca3d3ab1638a27ad01a43750
Van Carney <carney.van@gmail.com>
2015-07-30 17:22:03 -0700

    commiting updates / new rules file format

--- test/server/server/boot/api-hero.js
+++ test/server/server/boot/api-hero.js
@@ -1,7 +1,4 @@
 var hero = require('api-hero');
-var path = require('path');
 module.exports = function(app) {
-	// global.app_root = path.join(__dirname, '..', '..');
-	console.log("global.app_root: "+global.app_root);
 	hero.init(app);
 };
